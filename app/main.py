918eecc2cb5229327bbd2b78683e8d2c06e4f374
pawelkuczek <pawel.kuczek@vp.pl>
2026-06-05 15:36:18 +0200

    feat: add POST and GET endpoints for birthdays

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from app.schemas import Birthday, BirthdayCreate
+from starlette.status import HTTP_201_CREATED
 
 app = FastAPI(
     title="Birthday Tracker API",
@@ -13,3 +14,17 @@ fake_id_counter = 1
 @app.get("/")
 async def get_root() -> dict:
     return {"message": "Witaj w Birthday Tracker API!"}
+
+@app.post("/birthdays", response_model=Birthday, status_code=HTTP_201_CREATED)
+async def create_birthday(birthday_in: BirthdayCreate):
+    global fake_id_counter
+    new_birthday = Birthday(id=fake_id_counter, **birthday_in.model_dump())
+
+    fake_database.append(new_birthday)
+    fake_id_counter += 1
+
+    return new_birthday
+
+@app.get("/birthdays", response_model=list[Birthday])
+async def get_birthdays():
+    return fake_database
