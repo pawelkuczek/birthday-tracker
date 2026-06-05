92d2050c9f2292247c6beb3e1ed336ef8af3e210
pawelkuczek <pawel.kuczek@vp.pl>
2026-06-05 18:05:08 +0200

    fix typo in schemas.py

--- app/schemas.py
+++ app/schemas.py
@@ -6,6 +6,5 @@ class BirthdayCreate(BaseModel):
     surname: str = Field(..., min_length=2, max_length=50, description="Nazwisko solenizanta/solenizantki")
     date_of_birth: date = Field(..., description="Data urodzenia w formacie YYYY-MM-DD")
 
-# Pełny model, który zwracamy do klienta (dziedziczy pola z BirthdayCreate i dodaje ID)
 class Birthday(BirthdayCreate):
     id: int
