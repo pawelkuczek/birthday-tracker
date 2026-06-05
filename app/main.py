01e799f9c1ba364e54e3cea735741768289b53e3
pawelkuczek <pawel.kuczek@vp.pl>
2026-06-05 17:50:59 +0200

    feat: implement PUT and DELETE for full in-memory CRUD

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status, HTTPException
 from app.schemas import Birthday, BirthdayCreate
 from starlette.status import HTTP_201_CREATED
 
@@ -28,3 +28,25 @@ async def create_birthday(birthday_in: BirthdayCreate):
 @app.get("/birthdays", response_model=list[Birthday])
 async def get_birthdays():
     return fake_database
+
+@app.put("/birthdays/{birthday_id}", response_model=Birthday)
+async def update_birthday(birthday_id: int, birthday_in: BirthdayCreate):
+    existing_birthday = next((b for b in fake_database if b.id == birthday_id), None)
+
+    if not existing_birthday:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Nie znaleziono urodzin o podanym ID")
+
+    existing_birthday.name = birthday_in.name
+    existing_birthday.surname = birthday_in.surname
+    existing_birthday.date_of_birth = birthday_in.date_of_birth
+    return existing_birthday
+
+@app.delete("/birthdays/{birthday_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_birthday(birthday_id: int):
+    existing_birthday = next((b for b in fake_database if b.id == birthday_id), None)
+
+    if not existing_birthday:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Nie znaleziono urodzin o podanym ID")
+
+    fake_database.remove(existing_birthday)
+    return None
