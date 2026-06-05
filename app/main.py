3c548986b500fc5375eea558272050785c8c4345
pawelkuczek <97603443+pawelkuczek@users.noreply.github.com>
2026-06-05 18:17:13 +0200

    feat: implement in-memory CRUD (#1)

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status, HTTPException
+from app.schemas import Birthday, BirthdayCreate
+from starlette.status import HTTP_201_CREATED
 
 app = FastAPI(
     title="Birthday Tracker API",
@@ -6,6 +8,45 @@ app = FastAPI(
     version="0.1.0"
 )
 
+fake_database: list[Birthday] = []
+fake_id_counter = 1
+
 @app.get("/")
 async def get_root() -> dict:
     return {"message": "Witaj w Birthday Tracker API!"}
+
+@app.post("/birthdays", response_model=Birthday, status_code=HTTP_201_CREATED)
+async def create_birthday(birthday_in: BirthdayCreate):
+    global fake_id_counter
+    new_birthday = Birthday(id=fake_id_counter, **birthday_in.model_dump())
+
+    fake_database.append(new_birthday)
+    fake_id_counter += 1
+
+    return new_birthday
+
+@app.get("/birthdays", response_model=list[Birthday])
+async def get_birthdays():
+    return fake_database
+
+@app.put("/birthdays/{birthday_id}", response_model=Birthday)
+async def update_birthday(birthday_id: int, birthday_in: BirthdayCreate):
+    existing_birthday = next((b for b in fake_database if b.id == birthday_id), None)
+
+    if not existing_birthday:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Nie znaleziono urodzin o podanym ID")
+
+    existing_birthday.name = birthday_in.name
+    existing_birthday.surname = birthday_in.surname
+    existing_birthday.date_of_birth = birthday_in.date_of_birth
+    return existing_birthday
+
+@app.delete("/birthdays/{birthday_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_birthday(birthday_id: int):
+    existing_birthday = next((b for b in fake_database if b.id == birthday_id), None)
+
+    if not existing_birthday:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Nie znaleziono urodzin o podanym ID")
+
+    fake_database.remove(existing_birthday)
+    return None
---
+++ app/schemas.py
@@ -0,0 +1,10 @@
+from pydantic import BaseModel, Field
+from datetime import date
+
+class BirthdayCreate(BaseModel):
+    name: str = Field(..., min_length=2, max_length=50, description="Imię solenizanta/solenizantki")
+    surname: str = Field(..., min_length=2, max_length=50, description="Nazwisko solenizanta/solenizantki")
+    date_of_birth: date = Field(..., description="Data urodzenia w formacie YYYY-MM-DD")
+
+class Birthday(BirthdayCreate):
+    id: int
